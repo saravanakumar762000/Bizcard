8556532fb258e75549e81173ee0bc00a951f7b3d
Saravana <101510777+saravanakumar762000@users.noreply.github.com>
2023-04-11 02:54:45 -0400

    Update app.py

--- Bizard/app.py
+++ Bizard/app.py
@@ -1,14 +1,12 @@
 import streamlit as st
-import easyocr
-import cv2
-import numpy as np
 import mysql.connector
-import io
-from PIL import Image
 import cv2
-import base64
+import easyocr
+import urllib.request
+import numpy as np
+import pandas as pd
 
-# Set up MySQL connection
+
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -16,46 +14,119 @@ mydb = mysql.connector.connect(
   database="card_extraction"
 )
 
+# Create a table to store image and text
 mycursor = mydb.cursor()
+mycursor.execute("CREATE TABLE IF NOT EXISTS cards_detection6 (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255),job_title VARCHAR(255),phone1 VARCHAR(255),phone2 VARCHAR(255),website VARCHAR(255),email VARCHAR(255),address VARCHAR(255),company_name VARCHAR(255),image LONGBLOB)")
 
+# Set up EasyOCR reader
 reader = easyocr.Reader(['en'])
 
-mycursor.execute("CREATE TABLE IF NOT EXISTS business_card8 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), phone VARCHAR(255), address VARCHAR(255), image BLOB)")
+# Streamlit app
 
-uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
-menu = ['Insert Data', 'Display Data', 'Update Data', "Delete Data"]
-choose = st.sidebar.selectbox("Select An Option", menu)
+st.title("Extract Text from Image and Save to Database")
 
-if choose == 'Insert Data':
-  if uploaded_file is not None:
-    img_bytes = uploaded_file.read()
-    img = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_COLOR)
-    result = reader.readtext(img)
+uploaded_file = st.file_uploader("Upload a business card image", type=["jpg", "jpeg", "png"])
 
-        # Extract the relevant information from the OCR result
-    name, phone, email,address = "", "", "",""
-    for r in result:
-            text = r[1]
-            if 'name' not in name.lower() and any(x.isalpha() for x in text):
-                name = text
-            elif 'phone' not in phone.lower() and any(x.isdigit() for x in text):
-                phone = text
-            elif 'email' not in email.lower() and '@' in text:
-                email = text
-            elif "address" not in address.lower and 'st' in text:
-                address=text
-        # Display the extracted information
-    st.write("Name:", name)
-    st.write("Phone:", phone)
-    st.write("Email:", email)
+# Create a sidebar menu with options to add, view, update, and delete business card information
+menu = ['Insert', 'View', 'Update', 'Delete']
+choice = st.sidebar.selectbox("Select an option", menu)
 
-        # Save the image and information to the database
-    if st.button("Save"):
-        mycursor = mydb.cursor()
-        encoded_string = base64.b64encode(img_bytes).decode('utf-8')
-        sql = "INSERT INTO  business_card8 (name, phone, email,address, image) VALUES (%s, %s, %s, %s,%s)"
-        val = (name, phone, email,address,encoded_string)
-        mycursor.execute(sql, val)
+if choice == 'Insert':
+    if uploaded_file is not None:
+        # Read the image using OpenCV
+        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+        # Display the uploaded image
+        st.image(image, caption='Uploaded business card image', use_column_width=True)
+        # Create a button to extract information from the image
+        if st.button('Extract Information'):
+            # Call the function to extract the information from the image
+            bounds = reader.readtext(image, detail=0)
+            # Convert the extracted information to a string
+            text = "\n".join(bounds)
+            # Insert the extracted information and image into the database
+            sql = "INSERT INTO cards_detection6(name, job_title, phone1, phone2, website, email, address, company_name,image) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s,%s)"
+            val = (bounds[0], bounds[1], bounds[2], bounds[3], bounds[4], bounds[5], bounds[6], bounds[7],cv2.imencode('.jpg', image)[1].tobytes())
+            mycursor.execute(sql, val)
+            mydb.commit()
+            # Display a success message
+            st.success("Business card information added to database.")
+elif choice == 'View':
+    # Display the stored business card information
+    mycursor.execute("SELECT * FROM cards_detection6")
+    result = mycursor.fetchall()
+    df = pd.DataFrame(result, columns=['id','name', 'job_title', 'phone1', 'phone2', 'website', 'email', 'address', 'company_name','image'])
+    st.write(df)
+
+elif choice == 'Update':
+    # Create a dropdown menu to select a business card to update
+    mycursor.execute("SELECT id, name FROM cards_detection6")
+    result = mycursor.fetchall()
+    business_cards = {}
+    for row in result:
+        business_cards[row[1]] = row[0]
+    selected_card_name = st.selectbox("Select a business card to update", list(business_cards.keys()))
+    
+    # Get the current information for the selected business card
+    mycursor.execute("SELECT * FROM cards_detection6 WHERE name=%s", (selected_card_name,))
+    result = mycursor.fetchone()
+    mycursor.fetchall()
+
+    # Display the current information for the selected business card
+    st.write("Name:", result[1])
+    st.write("Job Title:", result[2])
+    st.write("Phone1:", result[3])
+    st.write("Phone2:", result[4])
+    st.write("Website:", result[5])
+    st.write("Email:", result[6])
+    st.write("Address:", result[7])
+    st.write("Comapny name:", result[8])
+
+    # Get new information for the business card
+    name = st.text_input("Name", result[1])
+    job_title = st.text_input("Job Title", result[2])
+    phone1 = st.text_input("Phone1", result[3])
+    phone2 = st.text_input("Phone2", result[4])
+    website = st.text_input("Website", result[5])
+    email = st.text_input("Email", result[6])
+    address = st.text_input("Address", result[7])
+    company_name = st.text_input("Company Name", result[8])
+
+    # Create a button to update the business card
+    if st.button("Update Business Card"):
+        # Update the information for the selected business card in the database
+        mycursor.execute("UPDATE cards_detection6 SET name=%s, job_title=%s, phone1=%s, phone2=%s, website=%s, email=%s, address=%s, company_name=%s WHERE name=%s", 
+                             (name, job_title, phone1, phone2, website, email, address, company_name, selected_card_name))
         mydb.commit()
-        st.success("Data saved successfully")
-        mycursor.close()
+        st.success("Business card information updated in database.")
+elif choice == 'Delete':
+    # Create a dropdown menu to select a business card to delete
+    mycursor.execute("SELECT id, name FROM cards_detection6")
+    result = mycursor.fetchall()
+    business_cards = {}
+    for row in result:
+        business_cards[row[0]] = row[1]
+    selected_card_id = st.selectbox("Select a business card to delete", list(business_cards.keys()), format_func=lambda x: business_cards[x])
+
+    # Get the name of the selected business card
+    mycursor.execute("SELECT name FROM cards_detection6 WHERE id=%s", (selected_card_id,))
+    result = mycursor.fetchone()
+    if result:
+      selected_card_name = result[0]
+
+    # Display the current information for the selected business card
+      st.write("Name:", selected_card_name)
+    # Display the rest of the information for the selected business card
+
+    # Create a button to confirm the deletion of the selected business card
+    if st.button("Delete Business Card"):
+        mycursor.execute("DELETE FROM cards_detection6 WHERE name=%s", (selected_card_name,))
+        mydb.commit()
+        st.success("Business card information deleted from database.")
+        
+        
+        
+        
+        
+        
+        
+        
